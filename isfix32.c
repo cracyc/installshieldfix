7c51eeb009de9148738392efb4a0ee84d6ca5445
cracyc <cracyc@users.noreply.github.com>
2023-02-15 19:26:35 -0600

    handle installers which run copies of themselves

--- isfix32.c
+++ isfix32.c
@@ -1,10 +1,13 @@
 #define WIN32_LEAN_AND_MEAN
-#define __USE_MINGW_ANSI_STDIO 0
 #include <windows.h>
 #include "detours.h"
 
 typedef LRESULT (WINAPI *SendMessageA_t)(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+typedef LRESULT (WINAPI *CreateProcessA_t)(LPCSTR name, LPSTR cmdline, LPSECURITY_ATTRIBUTES procattr, LPSECURITY_ATTRIBUTES thrdattr, BOOL inherit, DWORD cflags, LPVOID env, LPCSTR cdir, LPSTARTUPINFOA sinfo, LPPROCESS_INFORMATION pinfo);
+
 SendMessageA_t SendMessageAOrig;
+CreateProcessA_t CreateProcessAOrig;
+HMODULE hmod;
 
 LRESULT WINAPI SendMessageAHook(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 {
@@ -13,20 +16,31 @@ LRESULT WINAPI SendMessageAHook(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lpara
 	return SendMessageAOrig(hwnd, msg, wparam, lparam);
 }
 
+LRESULT WINAPI CreateProcessAHook(LPCSTR name, LPSTR cmdline, LPSECURITY_ATTRIBUTES procattr, LPSECURITY_ATTRIBUTES thrdattr, BOOL inherit, DWORD cflags, LPVOID env, LPCSTR cdir, LPSTARTUPINFOA sinfo, LPPROCESS_INFORMATION pinfo)
+{
+	HMODULE module;
+	char dllname[MAX_PATH];
+	GetModuleFileNameA(hmod, dllname, MAX_PATH);
+	return DetourCreateProcessWithDllExA(name, cmdline, procattr, thrdattr, inherit, cflags, env, cdir, sinfo, pinfo, dllname, CreateProcessAOrig);
+}
+
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
-
 	switch (fdwReason) {
 		case DLL_PROCESS_ATTACH:
 		{
+			hmod = (HMODULE)hinstDLL;
 			DisableThreadLibraryCalls(hinstDLL);
 			HMODULE user32 = GetModuleHandleA("user32.dll");
+			HMODULE kernel32 = GetModuleHandleA("kernel32.dll");
 			SendMessageAOrig = (SendMessageA_t)GetProcAddress(user32, "SendMessageA");
+			CreateProcessAOrig = (CreateProcessA_t)GetProcAddress(kernel32, "CreateProcessA");
 			DetourRestoreAfterWith();
 
 			DetourTransactionBegin();
 			DetourUpdateThread(GetCurrentThread());
 			DetourAttach(&(PVOID)SendMessageAOrig, SendMessageAHook);
+			DetourAttach(&(PVOID)CreateProcessAOrig, CreateProcessAHook);
 			LONG error = DetourTransactionCommit();
 
 			break;
@@ -36,6 +50,7 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 			DetourTransactionBegin();
 			DetourUpdateThread(GetCurrentThread());
 			DetourDetach(&(PVOID)SendMessageAOrig, SendMessageAHook);
+			DetourDetach(&(PVOID)CreateProcessAOrig, CreateProcessAHook);
 			LONG error = DetourTransactionCommit();
 		}
 	}
